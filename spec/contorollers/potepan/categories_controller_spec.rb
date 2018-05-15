23a9bae664ad054758985285f0d45dbf37d21ad8
Tackeyyyyyyyy <meteoreon@yahoo.co.jp>
2018-05-15 22:24:45 +0900

    [categories_controller_spec.rb] テスト項目

--- spec/contorollers/potepan/categories_controller_spec.rb
+++ spec/contorollers/potepan/categories_controller_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe Potepan::CategoriesController, type: :controller do
       expect(response).to have_http_status 200
     end
 
+    it 'showテンプレートを表示すること' do
+      expect(response).to render_template(:show)
+    end
+
+    it "@taxonが期待される値を持つこと" do
+      expect(assigns(:taxon)).to eq taxon
+    end
+
   end
 end

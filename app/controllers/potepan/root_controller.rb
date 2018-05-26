dd34ce659657ac5fadf4c503f59c0b9294ff6bed
Tackeyyyyyyyy <meteoreon@yahoo.co.jp>
2018-05-26 22:48:45 +0900

    [root_controller.rb]　コード整形

--- app/controllers/potepan/root_controller.rb
+++ app/controllers/potepan/root_controller.rb
@@ -1,6 +1,7 @@
 class Potepan::RootController < ApplicationController
   AVAILABLE_PRODUCTS_COUNT = 4
+
   def index
-    @featured_products =  Spree::Product.order(available_on: :desc).limit(AVAILABLE_PRODUCTS_COUNT)
+    @featured_products = Spree::Product.order(available_on: :desc).limit(AVAILABLE_PRODUCTS_COUNT)
   end
 end

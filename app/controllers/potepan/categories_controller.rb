1e43e41d7c0a3166af65512eeea4ff0da4cc7cf8
Tackeyyyyyyyy <meteoreon@yahoo.co.jp>
2018-06-02 16:29:51 +0900

    N + 1問題の解決

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -3,6 +3,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.includes(root: :children)
-    @products = Spree::Product.in_taxon(@taxon)
+    @products = @taxon.products.includes(master: [:images, :default_price])
   end
 end

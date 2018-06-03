7abda68eb7acfdfe39b29dc57734481c55fd866b
Tackeyyyyyyyy <meteoreon@yahoo.co.jp>
2018-06-04 00:01:20 +0900

    テスト追加

---
+++ spec/contorollers/potepan/root_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Potepan::RootController, type: :controller do
+  describe "index Action successfull" do
+    before do
+      get :index
+    end
+
+    it "ステータスコードが200になること" do
+      expect(response).to have_http_status 200
+    end
+
+    it 'indexテンプレートを表示すること' do
+      expect(response).to render_template(:index)
+    end
+  end
+end

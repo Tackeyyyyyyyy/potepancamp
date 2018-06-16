55e686fcb8fb54fb0fa81d57d65c905cf4325c3e
Tackeyyyyyyyy <meteoreon@yahoo.co.jp>
2018-06-16 17:09:32 +0900

    スコープにする

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,4 +2,7 @@ Spree::Product.class_eval do
   scope :related_products, -> (product) {
     joins(:taxons).where(spree_taxons: {id: product.taxons.ids}).where.not(id: product.id).distinct
   }
+  scope :include_price_and_image, -> {
+    includes(master: [:default_price, :images])
+  }
 end

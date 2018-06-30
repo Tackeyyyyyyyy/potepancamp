1f58f6c7146e6de690a5348c32d189ff12d6bceb
Tackeyyyyyyyy <meteoreon@yahoo.co.jp>
2018-07-01 00:37:14 +0900

    N+1問題の解決とDocの修正

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,14 +1,15 @@
 Spree::Product.class_eval do
-  scope :related_products, -> (product) {
+  scope :related, -> (product) {
     joins(:taxons).where(spree_taxons: {id: product.taxons.ids}).where.not(id: product.id).distinct
   }
   scope :include_price_and_image, -> {
     includes(master: [:default_price, :images])
   }
 
-  # @param [Object] product
-  # @return [Object]
-  def related_products(product)
-    Spree::Product.include_price_and_image.related_products(self)
+  # @param [int] size
+  # @return ActiveRecord::Relation
+  def related_products(size)
+    limit = size * 2
+    Spree::Product.include_price_and_image.related(self).limit(limit).sample(size)
   end
 end
